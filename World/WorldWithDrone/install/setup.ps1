fdf43217254038daae343e48f33aa2cb5b249fdf
Miguel A Hasegawa <Miguel.A.Hasegawa@student.uts.edu.au>
2025-09-22 10:57:26 +0000

    mapping

--- World/WorldWithDrone/install/setup.ps1
+++ World/WorldWithDrone/install/setup.ps1
@@ -24,7 +24,6 @@ function _colcon_prefix_chain_powershell_source_script {
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/student/ros2_ws/install\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/student/git/FIREWARDENBOT/World/WorldWithDrone/src/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
